ca6eef5d5ebc8b02c42614fc85c6d1bc303d32bc
Saira Rashid <saira.rashid1216@gmail.com>
2023-06-12 11:14:54 -0400

    Add project files.

---
+++ ParrotdiseShop.Core/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParrotdiseShop.Core.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
