c512f72cd5bd1520fd1a9f7a7994d284f045ea6c
Saira Rashid <saira.rashid1216@gmail.com>
2023-06-22 14:10:33 -0400

    Remove Status bar from TinyMcE rich text editor

---
+++ ParrotdiseShop.Web/wwwroot/js/products/productForm.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#productForm").on("click", ".js-validate-upload", validateFileUpload);
+});
+
+
+let validateFileUpload = function () {
+    let uploadBox = $("#uploadBox");
+
+    if (uploadBox.val() !== "") {
+        return true;
+    }
+
+    Swal.fire({
+        icon: 'error',
+        text: 'Please upload an image!'
+    });
+
+    return false;
+}
+
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+    statusbar: false
+});
---
+++ ParrotdiseShop.Web/wwwroot/js/products/products.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    let container = $('#Products');
+    loadDataTable(container);
+    container.on("click", ".js-delete", confirmDeleteProduct);
+});
+
+let loadDataTable = function (container) {
+    table = container.DataTable({
+        order: [[1, 'asc']],
+        ajax: {
+            url: '/admin/api/products',
+            dataSrc: ''
+        },
+        columns: [
+            {
+                data: 'name',
+                render: function (data, type, product) {
+                    return `<a href="/admin/products/edit/${product.id}">${data}</a>`;
+                }
+            },
+            {
+                data: 'sku'
+            },
+            {
+                data: 'unitPrice'
+            },
+            {
+                data: 'unitsInStock'
+            },
+            {
+                data: 'category.name'
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    return `<button class="btn btn-danger js-delete" data-product-id="${data}"><i class="bi bi-trash me-2"></i></button>`;
+                }
+            }
+        ]
+    });
+};
+
+let confirmDeleteProduct = function () {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            deleteProduct($(this));
+        }
+    })
+};
+
+let deleteProduct = function (button) {
+    let productId = button.attr("data-product-id");
+    $.ajax({
+        url: "/admin/api/products/" + productId,
+        method: "DELETE"
+    })
+        .done(function (data) {
+            table.ajax.reload();
+            toastr.success(data);
+        })
+        .fail(function (data) {
+            toastr.error(data);
+        });
+}
