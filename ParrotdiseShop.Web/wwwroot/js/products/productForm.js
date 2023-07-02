9e141de8287c5fcdf3cf849310b994af08526190
Saira Rashid <saira.rashid1216@gmail.com>
2023-07-01 22:33:01 -0400

    Modify styles

--- ParrotdiseShop.Web/wwwroot/js/products/productForm.js
+++ ParrotdiseShop.Web/wwwroot/js/products/productForm.js
@@ -20,7 +20,7 @@ let validateFileUpload = function () {
 
 tinymce.init({
     selector: 'textarea',
-    plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+    plugins: 'anchor autolink charmap codesample emoticons lists visualblocks wordcount',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | align lineheight | numlist bullist indent outdent',
     statusbar: false
 });
